6f42ccaf87b66d8096f6e17be30b527a3f6c2c95
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2019-11-22 09:55:07 +0000

    ejs attempt

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
+
+
 const path = require('path');
 const express = require('express'); 
 const app = express(); 
 const port = 8080;
 const cors = require('cors')
-const bodyParser = require('body-parser')
+
+const email = req.body.email
+const condition = req.body.condition
+const price = req.body.price 
+const description = req.body.description
+// const bodyParser = require('body-parser')
 const router = express.Router();
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public')); 
 app.use( express.static('views'))
 
-app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-// app.get('/', function (req,res) {
-//     res.send('index')
-// })
-app.get('/advert', function(req, res) {
-    res.send('advert');
-});
+// res.render ('/advert.ejs', { email: email, condition: condition, price: price, description: description});
 
+// app.get('/advert', function(req, res) {
+//     res.send('advert.ejs');
+// });
+// these two functions are not talking to each other
 
-app.post('/advert', function (req, res) {
+app.get('/advert.ejs', function (req, res) {
     console.log(req.body);
-    res.send(req.body)
+    res.render({email: email, condition: condition, price: price, description: description})
 });
 
 
