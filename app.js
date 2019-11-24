2bdfc6a1b14f3007dbf3ad30157d595d54974e66
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2019-11-24 19:45:54 +0000

    updates

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
-
-
 const path = require('path');
-const express = require('express'); 
-const app = express(); 
+const express = require('express');
+const app = express();
 const port = 8080;
-const cors = require('cors')
-
-const email = req.body.email
-const condition = req.body.condition
-const price = req.body.price 
-const description = req.body.description
-// const bodyParser = require('body-parser')
+const ejs = require('ejs')
 const router = express.Router();
+
 app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(cors());
-app.use(express.static('public')); 
-app.use( express.static('views'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(express.static('views'))
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.post('/advert', (req, res) => {
+    const email = req.body.email
+    const condition = req.body.condition
+    const price = req.body.price
+    const description = req.body.description
 
+    res.render('advert.ejs', {
+        data: {
+            email: email,
+            condition: condition,
+            price: price,
+            description: description
+        }
+    });
+})
 
-// res.render ('/advert.ejs', { email: email, condition: condition, price: price, description: description});
-
-// app.get('/advert', function(req, res) {
-//     res.send('advert.ejs');
-// });
-// these two functions are not talking to each other
-
-app.get('/advert.ejs', function (req, res) {
-    console.log(req.body);
-    res.render({email: email, condition: condition, price: price, description: description})
-});
 
 
 
